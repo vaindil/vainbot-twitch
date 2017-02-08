cdd7a3ef25472a083fe1787926dec94eda5052fc
Steven Hall <vaindil@gmail.com>
2017-02-08 18:29:38 -0500

    oh look it works

--- Program.cs
+++ Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using TwitchLib;
+using TwitchLib.Events.Client;
+using TwitchLib.Models.Client;
 
 namespace VainBotTwitch
 {
     class Program
     {
-        static void Main(string[] args)
+        static TwitchClient client;
+
+        static void Main(string[] args) => new Program().Run();
+
+        public void Run()
         {
+            var username = ConfigurationManager.AppSettings["twitchUsername"];
+            var oauth = ConfigurationManager.AppSettings["twitchOauth"];
+
+            client = new TwitchClient(new ConnectionCredentials(username, oauth), "crendor");
+
+            client.OnConnected += clientConnected;
+            client.OnJoinedChannel += clientJoinedChannel;
+
+            client.Connect();
+
+            Console.ReadLine();
+        }
+
+        void clientConnected(object sender, OnConnectedArgs e)
+        {
+            Console.WriteLine("Connected!");
+        }
+
+        void clientJoinedChannel(object sender, OnJoinedChannelArgs e)
+        {
+            client.SendMessage(client.GetJoinedChannel(e.Channel), "Oh look it works.");
+            Console.WriteLine($"Joined channel {e.Channel}");
         }
     }
 }
