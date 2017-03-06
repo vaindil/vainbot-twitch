779f2ce3de5caca831653843f2617b9d91160193
Steven Hall <vaindil@gmail.com>
2017-03-06 14:21:06 -0500

    EF set up

---
+++ Classes/SlothyRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace VainBotTwitch.Classes
+{
+    public class SlothyRecord
+    {
+        public long ChannelId { get; set; }
+
+        public long UserId { get; set; }
+
+        public decimal Count { get; set; }
+    }
+}
---
+++ VbContext.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.Entity;
+using System.Data.Entity;
+using VainBotTwitch.Classes;
+
+namespace VainBotTwitch
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class VbContext : DbContext
+    {
+        public VbContext() : base("name=VbContext") { }
+
+        public virtual DbSet<SlothyRecord> Slothies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SlothyRecord>()
+                .ToTable("slothy_record")
+                .HasKey(r => new { r.ChannelId, r.UserId });
+
+            modelBuilder.Entity<SlothyRecord>()
+                .Property(r => r.ChannelId)
+                .HasColumnName("channel_id");
+
+            modelBuilder.Entity<SlothyRecord>()
+                .Property(r => r.UserId)
+                .HasColumnName("user_id");
+
+            modelBuilder.Entity<SlothyRecord>()
+                .Property(r => r.Count)
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
